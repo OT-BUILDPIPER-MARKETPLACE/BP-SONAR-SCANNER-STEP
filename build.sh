34a388fe1033e751f5e4fc7955154311a2e57625
Sandeep <sandeep@opstree.com>
2022-10-17 17:33:15 +0530

    Updated code to process the scanning result

--- build.sh
+++ build.sh
@@ -8,3 +8,18 @@ cd  $WORKSPACE/${CODEBASE_DIR}
 logInfoMessage "I've recieved below arguments [$@]"
 
 sonar-scanner $@
+
+if [ $? -eq 0 ]
+then
+  logInfoMessage "Congratulations sonar scan succeeded!!!"
+  generateOutput mvn_execute true "Congratulations sonar scan succeeded!!!"
+elif [ $VALIDATION_FAILURE_ACTION == "FAILURE" ]
+  then
+    logErrorMessage "Please check sonar scan failed!!!"
+    generateOutput mvn_execute false "Please check sonar scan failed!!!!!"
+    echo "build unsucessfull"
+    exit 1
+   else
+    logWarningMessage "Please check sonar scan failed!!!"
+    generateOutput mvn_execute true "Please check sonar scan failed!!!!!"
+fi
