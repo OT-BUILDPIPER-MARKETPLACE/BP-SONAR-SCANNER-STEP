49ef12daa74e6e4200f0b3dda0870ffe068bfa4f
Sandeep <sandeep@opstree.com>
2022-10-17 17:09:09 +0530

    Adding capability to receive arguments

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ source functions.sh
 
 logInfoMessage "I'll scan the code available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
 sleep  $SLEEP_DURATION
+
+logInfoMessage "I've recieved below arguments [$@]"
