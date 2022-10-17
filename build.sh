cc8be91d1bee663a7e75c79f01227e23fc26a250
Sandeep <sandeep@opstree.com>
2022-10-17 17:27:29 +0530

    Updated code to go to dir where code actually exists

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ source functions.sh
 
 logInfoMessage "I'll scan the code available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
 sleep  $SLEEP_DURATION
+cd  $WORKSPACE/${CODEBASE_DIR}
 
 logInfoMessage "I've recieved below arguments [$@]"
 
