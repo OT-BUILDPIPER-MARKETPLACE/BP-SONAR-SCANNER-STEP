5c590b9d837d522a6333591bc61de1b32d248e2f
priyanshichauhan0707 <priyanshichauhan0707@gmail.com>
2022-12-22 18:39:52 +0530

    Changed BP-BASE-SHELL-STEPS to v0.4 version and added log-function in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source functions.sh
+source log-functions.sh
 
 logInfoMessage "I'll scan the code available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
 sleep  $SLEEP_DURATION
