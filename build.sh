8239f1989d3418c9944cbf65d5265d34d4d45a8e
Aayush Verma <aayush.verma@opstree.com>
2025-12-15 15:12:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,10 @@ source /opt/buildpiper/shell-functions/aws-functions.sh
 source /opt/buildpiper/shell-functions/getDataFile.sh
 source getDynamicVars.sh
 
+if [ "$DEBUG" = true ]; then
+  set -x
+fi
+
 git config --global --add safe.directory $WORKSPACE
 
 # Initialize task status
